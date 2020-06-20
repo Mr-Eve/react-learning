a3c7b5502a226e9603a3a397dd758b5323aa5ee4
Eve <948832626@qq.com>
2020-06-20 20:48:22 +0800

    feat: 通过双重循环渲染棋盘

--- src/index.js
+++ src/index.js
@@ -34,32 +34,29 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
-
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+    let row = [0, 1, 2];
+    let cell = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
+    ];
+    let checkerboard = row.map((item, index) => (
+      <div className="board-row" key={index}>
+        {cell.map((item1, index1) =>
+          index === index1
+            ? item1.map((item2, index) => this.renderSquare(item2))
+            : ''
+        )}
       </div>
-    );
+    ));
+    return checkerboard;
   }
 }
 
