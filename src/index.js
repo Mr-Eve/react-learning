67ad84f890288573beeca71de475334807394c89
Eve <948832626@qq.com>
2020-06-20 21:33:57 +0800

    refactor: 使用 clsx 替换 classnames

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import classnames from 'classnames';
+import clsx from 'clsx';
 
 function Square(props) {
   const { winSquare } = props;
   return (
     <button
-      className={classnames({
+      className={clsx({
         square: true,
         'strong-unit': winSquare
       })}
